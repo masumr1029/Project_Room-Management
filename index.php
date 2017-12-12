28ca555f84165bf1af829387662ee12e0d064445
Masum Rana <34295491+masumr1029@users.noreply.github.com>
2017-12-12 23:48:37 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">
+                	Room4You
+                </a>
+            </div>
+          
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                <li class="active">
+                        <a href="">Home</a>
+                    </li>
+                    <li >
+                        <a href="">About</a>
+                    </li>
+                    <li>
+                        <a href="">Services</a>
+                    </li>
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Category <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                            <li><a href="#">Meeting</a></li>
+                            <li><a href="#">Classroom</a></li>
+                            <li><a href="#">Office</a></li>
+                            <li><a href="#">Laboratory</a></li>
+                    </ul>
+                </li>
+                <li>
+                        <a href="">Contact</a>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+        <li><a href=""><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href=""><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+                
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
+
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                Reserve Now
+                </h1>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-6 portfolio-item">
+                <a href="">
+                    <img class="img-responsive" src="img/oi.jpg" alt="">
+                </a>
+                <h3>
+                    <a href="">Meeting</a>
+                </h3>
+                <p>A conference hall or conference room is a room provided for singular events such as business conferences and meetings. It is commonly found at large hotels and convention centers though many other establishments</p>
+            </div>
+            <div class="col-md-6 portfolio-item">
+                <a href="">
+                    <img class="img-responsive" src="img/office.jpg" alt="">
+                </a>
+                <h3>
+                    <a href="">Office</a>
+                </h3>
+                <p>An office is generally a room or other area where administrative work is done, but may also denote a position within an organization with specific duties attached to it </p>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-md-6 portfolio-item">
+                <a href="">
+                    <img class="img-responsive" src="img/class.jpeg" alt="">
+                </a>
+                <h3>
+                    <a href="">Classroom</a>
+                </h3>
+                <p>A classroom is a learning space, a room in which both children and adults learn about things. Classrooms are found in educational institutions of all kinds, from preschools to universities, and may also be found in other places where education or training is provided</p>
+            </div>
+            <div class="col-md-6 portfolio-item">
+                <a href="">
+                    <img class="img-responsive" src="img/lab.jpg" alt="" height="500px">
+                </a>
+                <h3>
+                    <a href="">Laboratory</a>
+                </h3>
+                <p>A laboratory is a facility that provides controlled conditions in which scientific or technological research, experiments, and measurement may be performed.Laboratories used for scientific the differing requirements of specialists in the various fields of science and engineering.</p>
+            </div>
+        </div>
+
+        
+
+    </div>
+    
+	<footer>
+		<div class="footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-4 footer-item">
+						<img class="img-circle" src="img/pic.jpg" alt="" >
+						<h3>Our Stuff</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. sed do eiusmod tempor incididunt ut.</p>
+						<p><a class="btn btn-primary" href="">More </a></p>
+					</div>
+					<div class="col-sm-4 footer-item">
+						<img class="img-circle" src="img/pic.jpg" alt="">
+						<h3>Our Stuff</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. sed do eiusmod tempor incididunt ut</p>
+						<p><a class="btn btn-primary" href="">More </a></p>
+					</div>
+					<div class="col-sm-4 footer-item">
+						<img class="img-circle" src="img/pic.jpg" alt="">
+						<h3>Our Stuff</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. sed do eiusmod tempor incididunt ut.</p>
+						<p><a class="btn btn-primary" href="#">More </a></p>
+					</div>
+
+				</div>
+					
+			</div>
+        </div>
+        
+        <div class="copyright">
+        	<div class="container">
+        		<p>&copy;Copyright  2017</p>
+        	</div>
+        </div>
+	</footer>
+
+	
+    <!-- jQuery -->
+    <script src="js/jquery-1.11.3.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
